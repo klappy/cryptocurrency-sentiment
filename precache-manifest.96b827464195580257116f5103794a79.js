0ef140f13234282c86b055b58a97796c5ee10969
Christopher Klapp <christopher@klapp.name>
2018-10-28 19:40:53 -0400

    Updates

---
+++ precache-manifest.96b827464195580257116f5103794a79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "183db95482a2f135f6ff",
+    "url": "/static/js/main.183db954.chunk.js"
+  },
+  {
+    "revision": "98bad0fe89841289dc18",
+    "url": "/static/js/1.98bad0fe.chunk.js"
+  },
+  {
+    "revision": "183db95482a2f135f6ff",
+    "url": "/static/css/main.7403b398.chunk.css"
+  },
+  {
+    "revision": "a112db0a7ee3a991b9cff6e41e78b48a",
+    "url": "/index.html"
+  }
+];
