f636bd778760eeb111ad9b91b1c6358282f298ab
Christopher Klapp <christopher@klapp.name>
2018-11-08 21:25:09 -0500

    Updates

---
+++ precache-manifest.8c78192018687a3bdde8c236f6eb73e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56ab470a560bfd15e056",
+    "url": "/cryptocurrency-sentiment/static/css/main.39b352c1.chunk.css"
+  },
+  {
+    "revision": "56ab470a560bfd15e056",
+    "url": "/cryptocurrency-sentiment/static/js/main.56ab470a.chunk.js"
+  },
+  {
+    "revision": "578c1633501cdd1c66aa",
+    "url": "/cryptocurrency-sentiment/static/js/1.578c1633.chunk.js"
+  },
+  {
+    "revision": "32055402b020bdb3f17f",
+    "url": "/cryptocurrency-sentiment/static/js/runtime~main.32055402.js"
+  },
+  {
+    "revision": "fac5d94062a4c8e1bd328e04d009e71e",
+    "url": "/cryptocurrency-sentiment/index.html"
+  }
+];
